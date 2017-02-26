487d8dd8350f242f750fca4ee4f7730209bfbb06
Arvin <vireshsoedhwa@gmail.com>
2017-02-26 11:57:13 -0800

    initial init

---
+++ fibonacci.c
@@ -0,0 +1,83 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 25, 2017
+ *      Author: arvin
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sysexits.h>
+
+long long *fibptr;
+long long *fibstart;
+
+void* fibonacci_process(void* arg)
+{
+	long long *limitptr = (long long*) arg;
+	long long limit = *limitptr;
+
+	fibptr = (long long*) malloc(limit * sizeof(long long));  //memory allocated using malloc
+	fibstart = (long long*) malloc(sizeof(long long));  //memory allocated using malloc
+
+	fibstart = fibptr;
+
+	int first = 0, second = 1, next;
+	for(long long i = 0; i <= limit; i++)
+	{
+		  if ( i <= 1 )
+			 next = i;
+		  else
+		  {
+			 next = first + second;
+			 first = second;
+			 second = next;
+		  }
+
+		*fibptr  = next;
+		fibptr++;
+	}
+
+	fibptr = fibstart;
+
+	pthread_exit(0);
+}
+
+
+
+int main(int argc, char **argv )
+{
+	if(argc < 2)
+	{
+		printf("Usage: %s <num>\n", argv[0]);
+		exit(EX_USAGE);
+	}
+
+	long long *limit = malloc(sizeof(*limit));
+	*limit = atoll(argv[1]);
+
+	//Thread ID;
+	pthread_t tid;
+
+	//Create attributes
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	pthread_create(&tid, &attr, fibonacci_process, limit);
+
+	//wait until thread is done with work
+
+	pthread_join(tid, NULL);
+
+	printf("-------------\n");
+
+	for(long long i = 0; i <= *limit; i++)
+	{
+		printf("fibonacci for limit %i is %lld\n", i, *fibptr);
+		fibptr++;
+	}
+
+	return EXIT_SUCCESS;
+}
+
